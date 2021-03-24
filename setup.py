5a6512329f465998a849cbb0a49a82e32b6042c9
Stephen Berry <sberry@gravity-performance.com>
2021-03-24 12:24:29 -0400

    minor nit in the description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Send text messages on common printer events"""
+plugin_description = """Send text/email messages on common printer events"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Stephen Berry"
