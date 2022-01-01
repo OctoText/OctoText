0e040c1d59a8e7a4319a9233a9d81b6fd2c86a99
Stephen Berry <sberry@gravity-performance.com>
2022-01-01 09:02:40 -0500

    change for version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ plugin_package = "octoprint_OctoText"
 plugin_name = "OctoPrint-OctoText"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.2rc4"
+plugin_version = "0.3.2rc5"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
